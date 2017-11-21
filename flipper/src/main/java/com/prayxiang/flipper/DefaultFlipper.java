45170b1dc32ad976735b468d1cb55ec4df8f4513
xianggaofeng <xgf_nsn@163.com>
2017-11-21 19:51:02 +0800

    commit

--- flipper/src/main/java/com/prayxiang/flipper/DefaultFlipper.java
+++ flipper/src/main/java/com/prayxiang/flipper/DefaultFlipper.java
@@ -1,47 +1,46 @@
-package com.prayxiang.flipper;
-
-import android.support.v7.widget.RecyclerView;
-import android.view.View;
-import android.view.ViewGroup;
-
-import com.prayxiang.base.vo.Status;
-
-/**
- * Created by xianggaofeng on 2017/11/21.
- */
-
-public abstract class DefaultFlipper extends Flipper {
-    private View mProgressView;
-    private View mEmptyView;
-    private View mLoadingView;
-    private View mErrorView;
-    private View mCurrentView;
-    private View mContenView;
-
-    private Status mStatus;
-
-
-
-    private void checkStatusInternal(){
-        if(mCurrentView==null){
-            mCurrentView = mContenView;
-        }
-        if(mStatus==Status.SUCCESS && mContenView!=null){
-            replace(mCurrentView,mContenView);
-        }else if (mStatus==Status.ERROR&&mErrorView!=null){
-            replace(mCurrentView,mErrorView);
-        }else if(mStatus==Status.LOADING&&mLoadingView!=null){
-            replace(mCurrentView,mLoadingView);
-        }/*else if(mStatus==Status.EMPTY){
-
-        }*/
-    }
-
-    public void setStatus(Status status) {
-        if(status==mStatus){
-            return;
-        }
-        this.mStatus = mStatus;
-        checkStatusInternal();
-    }
-}
+//package com.prayxiang.flipper;
+//
+//import android.support.v7.widget.RecyclerView;
+//import android.view.View;
+//import android.view.ViewGroup;
+//
+//
+///**
+// * Created by xianggaofeng on 2017/11/21.
+// */
+//
+//public abstract class DefaultFlipper extends Flipper {
+//    private View mProgressView;
+//    private View mEmptyView;
+//    private View mLoadingView;
+//    private View mErrorView;
+//    private View mCurrentView;
+//    private View mContenView;
+//
+//    private Status mStatus;
+//
+//
+//
+//    private void checkStatusInternal(){
+//        if(mCurrentView==null){
+//            mCurrentView = mContenView;
+//        }
+//        if(mStatus==Status.SUCCESS && mContenView!=null){
+//            replace(mCurrentView,mContenView);
+//        }else if (mStatus==Status.ERROR&&mErrorView!=null){
+//            replace(mCurrentView,mErrorView);
+//        }else if(mStatus==Status.LOADING&&mLoadingView!=null){
+//            replace(mCurrentView,mLoadingView);
+//        }/*else if(mStatus==Status.EMPTY){
+//
+//        }*/
+//    }
+//
+//    public void setStatus(Status status) {
+//        if(status==mStatus){
+//            return;
+//        }
+//        this.mStatus = mStatus;
+//        checkStatusInternal();
+//    }
+//}
