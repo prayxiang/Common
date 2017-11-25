ab0edd3860aa5550345a03ca4b1cb004df4e8b8b
相高峰 <xianggaofeng@droi.com>
2017-11-25 16:12:05 +0800

    commit

--- RecyclerViewExtension/src/main/java/com/recyclerview/recyclerview/extension/tools/LoaderMoreViewBinder.java
+++ RecyclerViewExtension/src/main/java/com/recyclerview/recyclerview/extension/tools/LoaderMoreViewBinder.java
@@ -1,43 +1,62 @@
 package com.recyclerview.recyclerview.extension.tools;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import com.recyclerview.extension.R;
-import com.recyclerview.extension.databinding.MultiTpeViewBinderLoadMoreBinding;
-import com.recyclerview.recyclerview.extension.DataBoundViewBinder;
-import com.recyclerview.recyclerview.extension.DataBoundViewHolder;
+import com.recyclerview.recyclerview.extension.ItemViewBinder;
+import com.recyclerview.recyclerview.extension.ItemViewHolder;
 
 
 /**
  * Created by xianggaofeng on 2017/6/6.
  */
-public class LoaderMoreViewBinder extends DataBoundViewBinder<LoaderMore, MultiTpeViewBinderLoadMoreBinding> {
+public class LoaderMoreViewBinder extends ItemViewBinder<LoaderMore, ItemViewHolder> {
 
     private LoadMoreScrollListener listener;
 
     public LoaderMoreViewBinder(LoadMoreScrollListener listener) {
-        super(R.layout.multi_tpe_view_binder_load_more);
         this.listener = listener;
     }
 
     @Override
-    public void onDataBoundCreated(final DataBoundViewHolder<MultiTpeViewBinderLoadMoreBinding> vh) {
-        super.onDataBoundCreated(vh);
-        vh.itemView.setOnClickListener(new View.OnClickListener() {
+    public ItemViewHolder onCreateViewHolder(LayoutInflater inflater, ViewGroup parent) {
+
+        ItemViewHolder holder = new ItemViewHolder(inflater.inflate(R.layout.multi_tpe_view_binder_load_more, parent, false));
+        holder.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 listener.loadMore();
             }
         });
+        return holder;
     }
 
     @Override
-    public void bindItem(DataBoundViewHolder<MultiTpeViewBinderLoadMoreBinding> holder, LoaderMore item) {
-        holder.binding.setData(item);
+    public void onBindViewHolder(ItemViewHolder holder, LoaderMore item) {
+        checkStatus(holder, item);
     }
 
+    private void checkStatus(ItemViewHolder holder, LoaderMore item) {
+        holder.findViewById(R.id.load_default).setVisibility(View.GONE);
+        holder.findViewById(R.id.load_loading).setVisibility(View.GONE);
+        holder.findViewById(R.id.load_fail).setVisibility(View.GONE);
+        holder.findViewById(R.id.load_finish).setVisibility(View.GONE);
+        if (item.loadMoreStatus == LoaderMore.STATUS_LOADING) {
+            holder.findViewById(R.id.load_loading).setVisibility(View.VISIBLE);
+        } else if (item.loadMoreStatus == LoaderMore.STATUS_SUCCESS) {
+            holder.findViewById(R.id.load_finish).setVisibility(View.VISIBLE);
+        } else if (item.loadMoreStatus == LoaderMore.STATUS_DEFAULT) {
+            holder.findViewById(R.id.load_default).setVisibility(View.VISIBLE);
+        } else if (item.loadMoreStatus == LoaderMore.STATUS_FAIL) {
+            holder.findViewById(R.id.load_fail).setVisibility(View.VISIBLE);
+        }
+    }
+
+
     @Override
-    public void onViewDetachedFromWindow(DataBoundViewHolder<MultiTpeViewBinderLoadMoreBinding> viewHolder) {
+    public void onViewDetachedFromWindow(ItemViewHolder viewHolder) {
         super.onViewDetachedFromWindow(viewHolder);
         LoaderMore more = viewHolder.getItem();
         if (more.loadMoreStatus == LoaderMore.STATUS_LOADING) {
@@ -47,10 +66,7 @@ public class LoaderMoreViewBinder extends DataBoundViewBinder<LoaderMore, MultiT
     }
 
     @Override
-    public void onViewAttachedToWindow(DataBoundViewHolder<MultiTpeViewBinderLoadMoreBinding> viewHolder) {
+    public void onViewAttachedToWindow(ItemViewHolder viewHolder) {
         super.onViewAttachedToWindow(viewHolder);
-        LoaderMore more = viewHolder.getItem();
-        if (more.loadMoreStatus == LoaderMore.STATUS_LOADING) {
-        }
     }
 }
